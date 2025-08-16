32d4a75da88d582a63cc587fee9a59d2bdf8b282
Aya <1220020@student.birzeit.edu>
2025-08-16 21:05:19 +0300

    add arc to put all the bales insid it

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-print("This is first line for the balles code ")
-print("This is sec line to make sure that every thing right ")
-print("This is Thered line to make sure that every thing right ")
+import pygame
+import math 
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))  # Fill background black
+    # Draw a arc 
+    pygame.draw.arc(screen , (250,250,250) , (300, 200, 200, 200) , 0 , 3*math.pi/2 , 2)
+    pygame.display.flip()  # Update screen
+
+pygame.quit()
